b726b1669147afef0dc3907c8e1f5f569374c483
Njini1 <62535686+Njini1@users.noreply.github.com>
2025-12-16 20:07:42 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 572 ms, Memory: 142092 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,55 +1,51 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-  static List<Integer>[] graph;
-  static boolean[] visited;
+    static List<Integer>[] graph;
+    static boolean[] visited;
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    int N = Integer.parseInt(st.nextToken());
-    int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-    graph = new ArrayList[N + 1];
-    visited = new boolean[N + 1];
+        graph = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
 
-    for (int i = 1; i <= N; i++) {
-      graph[i] = new ArrayList<>();
+        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        int count = 0;
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 1; i <= N; i++) {
+            if (visited[i]) continue;
+
+            // 새 연결요소 시작
+            count++;
+            visited[i] = true;
+            stack.push(i);
+
+            while (!stack.isEmpty()) {
+                int cur = stack.pop();
+                for (int nxt : graph[cur]) {
+                    if (visited[nxt]) continue;
+                    visited[nxt] = true;
+                    stack.push(nxt);
+                }
+            }
+        }
+
+        System.out.println(count);
     }
-
-    for (int i = 0; i < M; i++) {
-      st = new StringTokenizer(br.readLine());
-      int u = Integer.parseInt(st.nextToken());
-      int v = Integer.parseInt(st.nextToken());
-
-      graph[u].add(v);
-      graph[v].add(u);
-    }
-
-    int count = 0;
-    for (int i = 1; i <= N; i++) {
-      if (!visited[i]) {
-        dfs(i);
-        count++;
-      }
-    }
-
-    System.out.println(count);
-  }
-
-  static void dfs(int u) {
-    visited[u] = true;
-    for (int v : graph[u]) {
-      if (!visited[v]) {
-        dfs(v);
-      }
-    }
-  }
 }
